315e88feb305323105e8552ab7edda5af49733be
Abinashkalita <abinashabinash734@gmail.com>
2022-08-30 23:39:56 +0530

    first commit

---
+++ Documents/2salary.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf("enter the salary");
+scanf("%d",&s);
+printf("your salary is %d",s);
+return 0;
+}
---
+++ Documents/4ifelse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter your salary");
+scanf("%d",&a);
+if(a>500)
+printf("congragulations for your first step towards success");
+else
+printf("work hard");
+return 0;
+}
+
